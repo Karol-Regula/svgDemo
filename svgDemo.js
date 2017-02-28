b840fa1219557f5e359caaceeef8e57ee2e52b17
Karol-Regula <kregula@stuy.edu>
2017-02-28 12:29:27 -0500

    Committing initial work from class.

---
+++ svgDemo.js
@@ -0,0 +1,11 @@
+var b = document.getElementById("button-clear");
+var svg = document.getElementById("svg");
+
+var c1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+c1.setAttribute("cx", "200");
+c1.setAttribute("r", "20");
+svg.appendChild(c1);
+
+function clear(event) {
+  
+}
